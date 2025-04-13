831129693c1716d0d828bf9bee2b8028f28e6db6
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-12 23:00:18 -0300

    view - home - minor lint

--- src/views/home.rs
+++ src/views/home.rs
@@ -5,18 +5,21 @@ use crate::models::todo_list::TodoList;
 use super::base::base_view;
 
 pub fn home_view(create_todo_url: String, todo_lists: Vec<TodoList>) -> Markup {
-    base_view(html! {
-        h1 { "Todo App" }
-        button hx-get=(create_todo_url) hx-swap="outerHTML #app" class="btn btn-primary" {
-            "New Todo Todo"
-        }
-        select class="form-select" aria-label="Default select example" {
-            @for todo_list in todo_lists {
-                option value={(todo_list.name)} {
-                    (todo_list.name)
+    base_view(
+        html! {
+            h1 { "Todo App" }
+            button hx-get=(create_todo_url) hx-swap="outerHTML" class="btn btn-primary" {
+                "New Todo List"
+            }
+            hr;
+            select class="form-select" aria-label="Default select example" {
+                @for todo_list in todo_lists {
+                    option value=(todo_list.name) {
+                        (todo_list.name)
+                    }
                 }
             }
-        }
 
-    })
+        },
+    )
 }
