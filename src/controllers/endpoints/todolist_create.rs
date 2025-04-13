b5c3dadabfa1ab730d533a905c6ca10911409447
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-12 23:01:15 -0300

    controllers - new todo list
    adjusting code to use the new INSERT based struct

--- src/controllers/endpoints/todolist_create.rs
+++ src/controllers/endpoints/todolist_create.rs
@@ -1,15 +1,19 @@
-use actix_web::{get, post, web::{self, Data, Form}, HttpRequest, HttpResponse, Responder};
+use actix_web::{
+    HttpRequest, HttpResponse, Responder, get, post,
+    web::{self, Data, Form},
+};
 use maud::{Markup, html};
 
-use crate::{controllers::{dao::todo_list::create_todo_list, database::DatabaseConnectionPool}, models::{forms::todolist_create::NewTodoList, todo_list::TodoList}};
+use crate::{
+    controllers::{dao::todo_list::{create_todo_list, research_todo_list}, database::DatabaseConnectionPool},
+    models::{forms::todolist_create::NewTodoList, todo_list::{TodoListCreate, TodoList}},
+    views::home::home_view,
+};
 
 // HTMLX
 
 #[get("/todo_list/create", name = "todolist-create")]
-async fn todolist_create(
-    req: HttpRequest,
-) -> impl Responder {
-
+async fn todolist_create(req: HttpRequest) -> impl Responder {
     let create_todo_url = match req.url_for::<Vec<String>, String>("todolist-create", vec![]) {
         Ok(url) => url.to_string(),
         Err(e) => {
@@ -37,20 +41,17 @@ async fn todolist_create_post(
     form: Form<NewTodoList>,
     db_pool: Data<DatabaseConnectionPool>,
 ) -> impl Responder {
-
     let new_todolist: NewTodoList = form.into_inner();
 
-    let todolist: TodoList = TodoList {
-        id: 0,
+    let todolist = TodoListCreate {
         name: new_todolist.name.clone(),
     };
 
-    let _ = web::block(move ||
-        create_todo_list(
-            &mut db_pool.get().unwrap(),
-            todolist,
-        ).unwrap()
-    );
+    let mut conn = db_pool.get().unwrap();
+
+    let x = web::block(move || create_todo_list(&mut conn, todolist).unwrap());
+
+    dbg!(&x.await.as_ref().unwrap());
 
     let create_todo_url = match req.url_for::<Vec<String>, String>("todolist-create", vec![]) {
         Ok(url) => url.to_string(),
@@ -60,15 +61,18 @@ async fn todolist_create_post(
         }
     };
 
-    let content: Markup = html! {
-        h2 { "New Todo" }
-        form id="todolist"
-        hx-post=(create_todo_url)
-        hx-swap="outerHTML settle:3s"
-        hx-target="#app" {
-            input type="text" id="name" name="name" placeholder="Todo List Name" class="form-control";
-            input type="submit" value="Create" class="btn btn-success";
-        }
-    };
+    let todo_list: Vec<TodoList> =
+        match web::block(move || research_todo_list(&mut db_pool.get().unwrap()).unwrap()).await {
+            Ok(todo_list) => todo_list,
+            Err(e) => {
+                eprintln!("Error fetching todo list: {}", e); // TODO remove me later
+                vec![]
+            }
+        };
+
+    dbg!(&todo_list);
+
+    let content: Markup = home_view(create_todo_url, todo_list);
+
     HttpResponse::Ok().body(content.into_string())
 }
